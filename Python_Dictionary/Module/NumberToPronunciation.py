c0faa7679898d917f8d99a0c9026221850739013
Daniel H <54770658+danielhamen@users.noreply.github.com>
2022-02-11 17:04:31 -0500

    Create NumberToPronunciation.py

---
+++ Python_Dictionary/Module/NumberToPronunciation.py
@@ -0,0 +1,10 @@
+def NumberToPronunciation(Number):
+  """
+  Converts "Number" to a pronounceable string
+  
+  args:
+    Number (int): The number to be converted
+  """
+  
+  if 0 >= int(Number) >= 999:
+    from Numbers_0_1000 import Numbers
